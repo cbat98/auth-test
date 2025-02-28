6ca68eab6b3884c1841cce783262ef14a8c8fd39
Charlie Batten <charlie.batten@microlise.com>
2025-02-28 17:50:26 +0000

    Update auth config

--- src/app/auth/auth.config.ts
+++ src/app/auth/auth.config.ts
@@ -2,11 +2,11 @@ import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
 export const authConfig: PassedInitialConfig = {
   config: {
-              authority: 'https://auth.chrl.uk/application/o/auth-test',
+              authority: 'https://auth.chrl.uk/application/o/auth-test/.well-known/openid-configuration',
               redirectUrl: window.location.origin,
               postLogoutRedirectUri: window.location.origin,
               clientId: 'dJoec3Wn8MXlQ4S2oTXayWKR59T4AwxQtLzFKRoD',
-              scope: 'openid profile email offline_access', // 'openid profile offline_access ' + your scopes
+              scope: 'openid', // 'openid profile offline_access ' + your scopes
               responseType: 'code',
               silentRenew: true,
               useRefreshToken: true,
