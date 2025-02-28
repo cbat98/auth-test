b47482cd5785d2f7b10ac64b15b464929f5c6940
Charlie Batten <charlie.batten@microlise.com>
2025-02-28 16:38:56 +0000

    Added log in/out buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   username = 'there';
   loggedIn: boolean = false;
+
+  logIn() {
+    this.loggedIn = true;
+    this.username = 'Charlie';
+  }
+
+  logOut() {
+    this.loggedIn = false;
+    this.username = 'there';
+  }
 }
