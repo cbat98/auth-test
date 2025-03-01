29dc944e1edb7c68ec039026b335834dd804ca0f
Charlie B <charlie@charliebatten.co.uk>
2025-03-01 13:23:41 +0000

    Remove module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import { LoginResponse, OidcSecurityService } from 'angular-auth-oidc-client';
 import { User } from './user';
 
 @Component({
@@ -15,35 +14,17 @@ export class AppComponent {
   loggingIn: boolean = false;
   loggedIn: boolean = false;
 
-  private readonly oidcSecurityService = inject(OidcSecurityService);
-
-  ngOnInit() {
-    this.oidcSecurityService
-      .checkAuth()
-      .subscribe((loginResponse: LoginResponse) => {
-        const { isAuthenticated, userData, accessToken, idToken, configId } = loginResponse;
-        this.loggingIn = false;
-        this.loggedIn = isAuthenticated;
-        if (isAuthenticated) {
-          this.user = {
-            email: userData.email,
-            name: userData.name,
-            given_name: userData.given_name,
-            preferred_username: userData.preferred_username,
-          };
-        }
-      });
-  }
-
   logIn() {
-    this.loggingIn = true;
-    this.oidcSecurityService.authorize();
+    this.loggedIn = true;
+    this.user = {
+      name: "Charlie Batten",
+      email: "c@chrl.uk",
+      given_name: "Charlie",
+      preferred_username: "chrl"
+    };
   }
 
   logOut() {
-    this.oidcSecurityService.logoff().subscribe((result) => {
-      console.log("logging out");
-      console.log(result);
-    });
+    this.loggedIn = false;
   }
 }
